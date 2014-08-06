b91d0712df39d22e8358583a835a12d57536fdac
srikantsarwa <srikant_hitachi@live.com>
2014-08-06 08:47:52 +0530

    Checking in the files
    Checking in

---
+++ js/my.js
@@ -0,0 +1,6 @@
+﻿var client = new WindowsAzure.MobileServiceClient(
+    "https://testhtmlms.azure-mobile.net/",
+    "sSFpLGkvOfebwDkIkgDsqtZLHclniD45"
+);
+var item = { text: "Awesome item" };
+client.getTable("Item").insert(item);
